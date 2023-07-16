a9abfc25d748b1194a720755bc9fbf71da1d1e0d
VianNateDave(s) <117189867+VianteTech@users.noreply.github.com>
2023-07-16 10:19:37 +0700

    Update check.js

--- check.js
+++ check.js
@@ -1,59 +1,63 @@
 let tar=window.location.href;
-tar = tar.replace(/%20/g, "")
-console.log(tar);
-switch(tar){
-    case tar.toLowerCase().includes("aboutme"):
-        switch(tar){
-            case tar.toLowerCase().includes("https://viantetech.github.io"):
-                location.replace("https://viantetech.github.io/aboutme");
-                break;
-            case tar.toLowerCase().includes("https://thuongdev.pages.dev"):
-                location.replace("https://thuongdev.pages.dev/aboutme");
-                break;
-        }
-    case tar.toLowerCase().includes("home"):
-        switch(tar){
-            case tar.toLowerCase().includes("https://viantetech.github.io"):
-                location.replace("https://viantetech.github.io/home");
-                break;
-            case tar.toLowerCase().includes("https://thuongdev.pages.dev"):
-                location.replace("https://thuongdev.pages.dev/home");
-                break;
-        }
-    case tar.toLowerCase().includes("contact"):
-        switch(tar){
-            case tar.toLowerCase().includes("https://viantetech.github.io"):
-                location.replace("https://viantetech.github.io/contacts");
-                break;
-            case tar.toLowerCase().includes("https://thuongdev.pages.dev"):
-                location.replace("https://thuongdev.pages.dev/contacts");
-                break;
-        }
-    case tar.toLowerCase().includes("myproject"):
-        switch(tar){
-            case tar.toLowerCase().includes("https://viantetech.github.io"):
-                location.replace("https://viantetech.github.io/myprojects");
-                break;
-            case tar.toLowerCase().includes("https://thuongdev.pages.dev"):
-                location.replace("https://thuongdev.pages.dev/myprojects");
-                break;
-        }
-    case tar.toLowerCase().includes("rick")||tar.toLowerCase().includes("roll"):
-        switch(tar){
-            case tar.toLowerCase().includes("https://viantetech.github.io"):
-                location.replace("https://viantetech.github.io/rickroll");
-                break;
-            case tar.toLowerCase().includes("https://thuongdev.pages.dev"):
-                location.replace("https://thuongdev.pages.dev/rickroll");
-                break;
-        }
-    case tar.toLowerCase().includes("readme"):
-        switch(tar){
-            case tar.toLowerCase().includes("https://viantetech.github.io"):
-                location.replace("https://viantetech.github.io/README");
-                break;
-            case tar.toLowerCase().includes("https://thuongdev.pages.dev"):
-                location.replace("https://thuongdev.pages.dev/README");
-                break;
-        }            
+tar = tar.replace(/%20/g, "");
+tar = tar.toLowerCase();
+if (tar.includes("aboutme")){
+    switch(tar){
+        case tar.includes("https://viantetech.github.io"):
+            window.location.replace("https://viantetech.github.io/aboutme");
+            break;
+        case tar.includes("https://thuongdev.pages.dev"):
+            window.location.replace("https://thuongdev.pages.dev/aboutme");
+            break;
+    }
+}
+else if (tar.includes("home")){
+    switch(tar){
+        case tar.includes("https://viantetech.github.io"):
+            window.location.replace("https://viantetech.github.io/home");
+            break;
+        case tar.includes("https://thuongdev.pages.dev"):
+            window.location.replace("https://thuongdev.pages.dev/home");
+            break;
+    }
+}
+else if (tar.includes("contact")){
+    switch(tar){
+        case tar.includes("https://viantetech.github.io"):
+            window.location.replace("https://viantetech.github.io/contacts");
+            break;
+        case tar.includes("https://thuongdev.pages.dev"):
+            window.location.replace("https://thuongdev.pages.dev/contacts");
+            break;
+    }
+}
+else if (tar.includes("myproject")){
+    switch(tar){
+        case tar.includes("https://viantetech.github.io"):
+            window.location.replace("https://viantetech.github.io/myprojects");
+            break;
+        case tar.includes("https://thuongdev.pages.dev"):
+            window.location.replace("https://thuongdev.pages.dev/myprojects");
+            break;
+    }
+}
+else if (tar.includes("rick")||tar.includes("roll")){
+    switch(tar){
+        case tar.includes("https://viantetech.github.io"):
+            window.location.replace("https://viantetech.github.io/rickroll");
+            break;
+        case tar.includes("https://thuongdev.pages.dev"):
+            window.location.replace("https://thuongdev.pages.dev/rickroll");
+            break;
+    }
+}
+else if (tar.includes("readme")){
+    switch(tar){
+        case tar.includes("https://viantetech.github.io"):
+            window.location.replace("https://viantetech.github.io/README");
+            break;
+        case tar.includes("https://thuongdev.pages.dev"):
+            window.location.replace("https://thuongdev.pages.dev/README");
+            break;
+    }
 }
