7c9721cd3ad2cf05ad801d8b9faa884967b424f8
VianNateDave(s) <117189867+VianteTech@users.noreply.github.com>
2023-07-16 10:38:58 +0700

    Update check.js

--- check.js
+++ check.js
@@ -1,64 +1,51 @@
 let tar=window.location.href;
 tar = tar.replace(/%20/g, "");
 tar = tar.toLowerCase();
-console.log(tar);
 if (tar.includes("aboutme")){
-    switch(tar){
-        case tar.includes("https://viantetech.github.io"):
-            location.replace("https://viantetech.github.io/aboutme");
-            break;
-        case tar.includes("https://thuongdev.pages.dev"):
-            location.replace("https://thuongdev.pages.dev/aboutme");
-            break;
+    if(tar.includes("https://viantetech.github.io")){
+        window.location.replace("https://viantetech.github.io/aboutme");
+    }
+    else if(tar.includes("https://thuongdev.pages.dev")){ 
+        window.location.replace("https://thuongdev.pages.dev/aboutme");
     }
 }
 else if (tar.includes("home")){
-    switch(tar){
-        case tar.includes("https://viantetech.github.io"):
-            location.replace("https://viantetech.github.io/home");
-            break;
-        case tar.includes("https://thuongdev.pages.dev"):
-            location.replace("https://thuongdev.pages.dev/home");
-            break;
+    if(tar.includes("https://viantetech.github.io")){             
+        window.location.replace("https://viantetech.github.io/home");
+    }
+    else if(tar.includes("https://thuongdev.pages.dev")){ 
+        window.location.replace("https://thuongdev.pages.dev/home");
     }
 }
 else if (tar.includes("contact")){
-    switch(tar){
-        case tar.includes("https://viantetech.github.io"):
-            location.replace("https://viantetech.github.io/contacts");
-            break;
-        case tar.includes("https://thuongdev.pages.dev"):
-            location.replace("https://thuongdev.pages.dev/contacts");
-            break;
+    if(tar.includes("https://viantetech.github.io")){   
+        window.location.replace("https://viantetech.github.io/contacts");
+    }
+    else if(tar.includes("https://thuongdev.pages.dev")){ 
+        window.location.replace("https://thuongdev.pages.dev/contacts");
     }
 }
 else if (tar.includes("myproject")){
-    switch(tar){
-        case tar.includes("https://viantetech.github.io"):
-            location.replace("https://viantetech.github.io/myprojects");
-            break;
-        case tar.includes("https://thuongdev.pages.dev"):
-            location.replace("https://thuongdev.pages.dev/myprojects");
-            break;
+    if(tar.includes("https://viantetech.github.io")){    
+        window.location.replace("https://viantetech.github.io/myprojects");
+    }
+    else if(tar.includes("https://thuongdev.pages.dev")){ 
+        window.location.replace("https://thuongdev.pages.dev/myprojects");
     }
 }
 else if (tar.includes("rick")||tar.includes("roll")){
-    switch(tar){
-        case tar.includes("https://viantetech.github.io"):
-            location.replace("https://viantetech.github.io/rickroll");
-            break;
-        case tar.includes("https://thuongdev.pages.dev"):
-            location.replace("https://thuongdev.pages.dev/rickroll");
-            break;
+    if(tar.includes("https://viantetech.github.io")){             
+        window.location.replace("https://viantetech.github.io/rickroll");
+    }
+    else if(tar.includes("https://thuongdev.pages.dev")){ 
+        window.location.replace("https://thuongdev.pages.dev/rickroll");
     }
 }
 else if (tar.includes("readme")){
-    switch(tar){
-        case tar.includes("https://viantetech.github.io"):
-            location.replace("https://viantetech.github.io/README");
-            break;
-        case tar.includes("https://thuongdev.pages.dev"):
-            location.replace("https://thuongdev.pages.dev/README");
-            break;
+    if(tar.includes("https://viantetech.github.io")){             
+        window.location.replace("https://viantetech.github.io/README");
+    }
+    else if(tar.includes("https://thuongdev.pages.dev")){ 
+        window.location.replace("https://thuongdev.pages.dev/README");
     }
 }
